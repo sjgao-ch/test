17bfc7823baa32a0c61154d0757d29a2954c0d99
高世杰 <gaosj10011@163.com>
2023-03-20 17:10:44 +0800

    测试

--- test.java
+++ test.java
@@ -1,7 +1,14 @@
 /**
  * @author gsj
+<<<<<<< HEAD
  * {@code @date} 2023/03/20 17:00
  * description 测试git合并和变基
  **/
 public class test {
+=======
+ * {@code @date} 2023/03/20 17:10
+ * description 测试接口
+ **/
+public interface test {
+>>>>>>> 0920cad (测试)
 }
