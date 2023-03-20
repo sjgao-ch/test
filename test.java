d86bdfd6d3cb1d8e6f0b7fa04acbf85a6b7aac10
高世杰 <gaosj10011@163.com>
2023-03-20 18:19:14 +0800

    main改正

--- test.java
+++ test.java
@@ -1,7 +1,10 @@
 /**
  * @author gsj
- * {@code @date} 2023/03/20 17:00
- * description 测试git合并和变基
+ * {@code @date} 2023/03/20 18:15
+ * description 测试
  **/
 public class test {
+    public static void main(String[] args) {
+        System.out.println("测试test");
+    }
 }
